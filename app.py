5ab4552ee8b8c5398bc9af494a6c5a3278111117
Kash <amahlw@gmail.com>
2020-11-04 11:20:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import random
 
 app = Flask(__name__)
 
+
 def sort_letters(message):
     """A helper method to sort the characters of a string in alphabetical order
     and return the new string."""
@@ -14,25 +15,36 @@ def homepage():
     """A homepage with handy links for your convenience."""
     return render_template('home.html')
 
+
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
+
 
 @app.route('/froyo_results')
 def show_froyo_results():
-    """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get('flavor')
+    return f'You ordered {users_froyo_flavor} flavored Fro-Yo!'
+
 
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
     pass
 
+
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
     pass
+
 
 @app.route('/secret_message')
 def secret_message():
@@ -40,10 +52,12 @@ def secret_message():
     the POST method to keep it a secret!"""
     pass
 
+
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
     pass
+
 
 @app.route('/calculator')
 def calculator():
@@ -63,14 +77,15 @@ def calculator():
     </form>
     """
 
+
 @app.route('/calculator_results')
 def calculator_results():
     """Shows the user the result of their calculation."""
     pass
 
 
-# List of compliments to be used in the `compliments_results` route (feel free 
-# to add your own!) 
+# List of compliments to be used in the `compliments_results` route (feel free
+# to add your own!)
 # https://systemagicmotives.com/positive-adjectives.htm
 list_of_compliments = [
     'awesome',
@@ -98,10 +113,12 @@ list_of_compliments = [
     'zoetic'
 ]
 
+
 @app.route('/compliments')
 def compliments():
     """Shows the user a form to get compliments."""
     return render_template('compliments_form.html')
+
 
 @app.route('/compliments_results')
 def compliments_results():
